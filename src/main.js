a7f6a7b2a8738eec48a7748160053349ecf1a623
Patryk <patrykkfydrych1998@gmail.com>
2024-06-23 20:06:00 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// src/main.js
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "/src/styles.css";
+document.querySelector("#app").innerHTML = `
+  <div class="container">
+    <h1 class="mt-5 text-center">Hello, Bootstrap!</h1>
+  </div>
+`;
